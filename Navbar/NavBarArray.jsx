f92a155933fe4aeeec1fd49f629356fee70e7fcd
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-10-29 22:14:51 -0300

    primer commit

---
+++ Navbar/NavBarArray.jsx
@@ -0,0 +1,39 @@
+const navBarItems = [
+    {
+        id: 1,
+        title: "Inicio",
+        link: "/",
+        animation: "fade-right",
+        duration: 200,
+    },
+    {
+        id: 2,
+        title: "Acerca de mí",
+        link: "/about",
+        animation: "fade-left",
+        duration: 300,
+    },
+    {
+        id: 3,
+        title: "Estudios",
+        link: "/education",
+        animation: "fade-right",
+        duration: 400,
+    },
+    {
+        id: 4,
+        title: "Proyectos",
+        link: "/projects",
+        animation: "fade-left",
+        duration: 500,
+    },
+    {
+        id: 5,
+        title: "Contacto",
+        link: "/contact",
+        animation: "fade-right",
+        duration: 600,
+    },
+]
+
+export default navBarItems;
